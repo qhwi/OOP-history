bf1aeecf0f933887fb872a6d7e4801160c5d7629
qhwi <nguyenquochuy712@gmail.com>
2023-07-02 18:13:55 +0700

    add time parse util for event crawl

--- oop/src/main/java/hust/soict/cysec/oop/crawler/utils/TimeUtility.java
+++ oop/src/main/java/hust/soict/cysec/oop/crawler/utils/TimeUtility.java
@@ -15,4 +15,44 @@ public class TimeUtility {
 		
 		return arr;
 	}
+	
+	public static ArrayList<String> extractYearRangeFromTitle(String str) {
+		// String in format of "Some event (-1000 - 1000)" or "Some event (200 TCN)"
+		str = str.strip();
+		String title = str;
+		String time = null;
+		String startTime = "?";
+		String endTime = "?";
+		Matcher year_matcher = Pattern.compile("\\(([^)]+)\\)[^()]*$").matcher(str);
+		ArrayList<String> ret = new ArrayList<String>();
+		
+		if (year_matcher.find()) {
+			time = year_matcher.group(1).replace("–", "-");
+			if (!time.contains(" - ")) {
+				startTime = time.replace(" ", "");
+				if (startTime.contains("-"))
+					startTime = startTime.replace("-", "") + " TCN";
+			}
+			else {
+				String[] arr = time.split(" - ", 2);
+				startTime = arr[0].replace(" ", "");
+				if (startTime.contains("-"))
+					startTime = startTime.replace("-", "") + " TCN";
+				if (!startTime.matches(".*\\d.*"))
+					startTime = "?";
+				endTime = arr[1].replace(" ", "");
+				if (endTime.contains("-"))
+					endTime = endTime.replace("-", "") + " TCN";
+			}
+			if (!startTime.matches(".*\\d.*"))
+				startTime = "?";
+			if (!endTime.matches(".*\\d.*"))
+				endTime = "?";
+			title = str.replaceAll("\\s*\\([^)]+\\)", "");
+		}
+		ret.add(title);
+		ret.add(startTime);
+		ret.add(endTime);
+		return ret;
+	}
 }
