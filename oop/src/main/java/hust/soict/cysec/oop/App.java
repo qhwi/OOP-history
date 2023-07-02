f20b2ecfe2b9bcd6ac4129e9fcbc9152de8aa58c
qhwi <nguyenquochuy712@gmail.com>
2023-07-02 18:41:53 +0700

    get jsons

--- oop/src/main/java/hust/soict/cysec/oop/App.java
+++ oop/src/main/java/hust/soict/cysec/oop/App.java
@@ -2,15 +2,21 @@ package hust.soict.cysec.oop;
 import java.io.IOException;
 
 import hust.soict.cysec.oop.crawler.*;
+import hust.soict.cysec.oop.crawler.dynasty.DynastyCrawler;
+import hust.soict.cysec.oop.crawler.event.EventCrawler;
+import hust.soict.cysec.oop.crawler.festival.FestivalCrawler;
+import hust.soict.cysec.oop.crawler.relic.RelicFinal;
 
-public class App 
-{
-    public static void main( String[] args ) throws IOException
-    {
-    	System.out.println("Crawling Dynasties...");
+public class App {
+	public static void main( String[] args ) throws IOException {
+    	System.out.println("\n>>\n>> Crawling Dynasties...\n>>\n");
     	DynastyCrawler.main(args);
-    	System.out.println("Crawling Festivals...");
+    	System.out.println("\n>>\n>> Crawling Festivals...\n>>\n");
     	FestivalCrawler.main(args);
-        System.out.println("DONE!");
+    	System.out.println("\n>>\n>> Crawling Events...\n>>\n");
+    	EventCrawler.main(args);
+		System.out.println("\n>>\n>> Crawling Relics...\n>>\n");
+		RelicFinal.main(args);
+        System.out.println("\n>>DONE!");
     }
 }
