120d19952d704779c485601259e713391f4cc8eb
Riesling1623 <ninh.nguyn1602@gmail.com>
2023-07-08 16:00:57 +0700

    Update

--- oop/src/main/java/hust/soict/cysec/oop/application/MainController.java
+++ oop/src/main/java/hust/soict/cysec/oop/application/MainController.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import hust.soict.cysec.oop.model.*;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
@@ -24,6 +25,9 @@ public class MainController {
 
     @FXML
     private VBox figureItem;
+    
+    @FXML
+    private VBox kingItem;
 
     @FXML
     private VBox relicItem;
@@ -36,22 +40,28 @@ public class MainController {
     
     private VBox[] items;
     private VBox[] selectedItem;
-//    private ObservableList<HistoricalFigure> historicalFigureList;
+    private ObservableList<Figure> figureList;
+    private ObservableList<King> kingList;
     private ObservableList<Relic> relicList;
-    private ObservableList<HistoricalEvent> historicalEventList;
+    private ObservableList<HistoricalEvent> eventList;
     private ObservableList<Dynasty> dynastyList;
     private ObservableList<Festival> festivalList;
     
-//    private TableView<HistoricalFigure> historicalFigureTable = new TableView<>();
+    private TableView<Figure> figureTable = new TableView<>();
+    private TableView<King> kingTable = new TableView<>();
     private TableView<Relic> relicTable = new TableView<>();
-    private TableView<HistoricalEvent> historicalEventTable = new TableView<>();
+    private TableView<HistoricalEvent> eventTable = new TableView<>();
     private TableView<Dynasty> dynastyTable = new TableView<>();
     private TableView<Festival> festivalTable = new TableView<>();
     
+    private List<String> figureTableFieldName = Arrays.asList("Tên", "Năm sinh", "Năm mất", "Bí danh", "Quê quán", "Ghi chú");
+    private List<String> figureTableFieldProperty = Arrays.asList("name", "birth", "death", "alias", "hometown", "note");
+    private List<String> kingTableFieldName = Arrays.asList("Tên", "Năm sinh", "Năm mất", "Miếu hiệu", "Thụy hiệu", "Niên hiệu", "Tên húy", "Thế thứ", "Năm trị vì", "Tiền nhiệm", "Kế nhiệm");
+    private List<String> kingTableFieldProperty = Arrays.asList("name", "birth", "death", "templateName", "posthumousName", "eraName", "courtesyName", "successionOrder", "reignYear", "predecessor", "successor");
     private List<String> relicTableFieldName = Arrays.asList("Tên", "Địa điểm", "Loại hình","Xếp hạng", "Mô tả");
     private List<String> relicTableFieldProperty = Arrays.asList("name", "location", "type", "rank", "desc");
-    private List<String> historicalEventTableFieldName = Arrays.asList("Tên", "Bắt đầu", "Kết thúc", "Mô tả");
-    private List<String> historicalEventTableFieldProperty = Arrays.asList("name", "startYear", "endYear", "desc");
+    private List<String> eventTableFieldName = Arrays.asList("Tên", "Bắt đầu", "Kết thúc", "Mô tả");
+    private List<String> eventTableFieldProperty = Arrays.asList("name", "startYear", "endYear", "desc");
     private List<String> dynastyTableFieldName = Arrays.asList("Tên", "Bắt đầu", "Kết thúc", "Thủ đô");
     private List<String> dynastyTableFieldProperty = Arrays.asList("name", "startYear", "endYear", "capital");
     private List<String> festivalTableFieldName = Arrays.asList("Tên", "Địa điểm", "Thời gian", "Mô tả");
@@ -60,7 +70,27 @@ public class MainController {
     @SuppressWarnings("unchecked")
     @FXML
     private void initialize() {
+    	// Data
+    	MainModel model = new MainModel();
+    	figureList = FXCollections.observableArrayList(model.getFigures());
+    	kingList = FXCollections.observableArrayList(model.getKings());
+    	relicList = FXCollections.observableArrayList(model.getRelics());
+    	eventList = FXCollections.observableArrayList(model.getEvents());
+    	dynastyList = FXCollections.observableArrayList(model.getDynasties());
+    	festivalList = FXCollections.observableArrayList(model.getFestivals());
     	
+    	tableData.getColumns().clear();
+    	settingTable();
+    }
+    
+    private <T> void settingTable(TableView<T> table, ObservableList<T> data, List<String> columnName, List<String> columnProperty) {
+    	table.setItems(data);
+    	for (int i = 0; i < columnName.size(); ++i) {
+    		TableColumn<T, ?> column = new TableColumn<>(columnName.get(i));
+    		column.prefWidthProperty().bind(tableData.widthProperty().multiply((1 - 0.1) / (columnName.size() - 1)));
+    		column.setCellValueFactory(new PropertyValueFactory<>(columnProperty.get(i)));
+    		table.getColumns().add(column);
+    	}
     }
     
     
---
+++ oop/src/main/java/hust/soict/cysec/oop/model/MainModel.java
@@ -0,0 +1,91 @@
+package hust.soict.cysec.oop.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import com.google.gson.Gson;
+
+public class MainModel {
+	private List<Dynasty> dynasties = new ArrayList<>();
+	private List<Festival> festivals = new ArrayList<>();
+	private List<HistoricalEvent> events = new ArrayList<>();
+	private List<Figure> figures = new ArrayList<>();
+	private List<King> kings = new ArrayList<>();
+	private List<Relic> relics = new ArrayList<>();
+	
+	public MainModel() {
+		try {
+			Gson gson = new Gson();
+			
+			Dynasty dynastyTemp[] = null;
+			FileReader readerDynasty = new FileReader("oop/src/main/json/Dynasty.json");
+			dynastyTemp = gson.fromJson(readerDynasty, Dynasty[].class);
+			for (Dynasty dynasty : dynastyTemp) {
+				dynasties.add(dynasty);
+			}
+			
+			Festival festivalTemp[] = null;
+			FileReader readerFestival = new FileReader("oop/src/main/json/Festival.json");
+			festivalTemp = gson.fromJson(readerFestival, Festival[].class);
+			for (Festival festival : festivalTemp) {
+				festivals.add(festival);
+			}
+			
+			HistoricalEvent eventTemp[] = null;
+			FileReader readerEvent = new FileReader("oop/src/main/json/Event.json");
+			eventTemp = gson.fromJson(readerEvent, HistoricalEvent[].class);
+			for (HistoricalEvent event : eventTemp) {
+				events.add(event);
+			}
+			
+			Figure figureTemp[] = null;
+			FileReader readerFigure = new FileReader("oop/src/main/json/Figures.json");
+			figureTemp = gson.fromJson(readerFigure, Figure[].class);
+			for (Figure figure : figureTemp) {
+				figures.add(figure);
+			}
+			
+			King kingTemp[] = null;
+			FileReader readerKing = new FileReader("oop/src/main/json/Kings.json");
+			kingTemp = gson.fromJson(readerKing, King[].class);
+			for (King king : kingTemp) {
+				kings.add(king);
+			}
+			
+			Relic relicTemp[] = null;
+			FileReader readerRelic = new FileReader("oop/src/main/json/Relic.json");
+			relicTemp = gson.fromJson(readerRelic, Relic[].class);
+			for (Relic relic : relicTemp) {
+				relics.add(relic);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Dynasty> getDynasties() {
+		return dynasties;
+	}
+	
+	public List<Festival> getFestivals() {
+		return festivals;
+	}
+	
+	public List<HistoricalEvent> getEvents() {
+		return events;
+	}
+	
+	public List<Figure> getFigures() {
+		return figures;
+	}
+	
+	public List<King> getKings() {
+		return kings;
+	}
+	
+	public List<Relic> getRelics() {
+		return relics;
+	}
+}
