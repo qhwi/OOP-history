5f17ccc418e842ce8688bb8e2586d88050033d03
ngocvu2405 <ngoc.vh.hust2003@gmail.com>
2023-07-09 12:39:51 +0700

    something update about GUI

--- oop/src/main/java/hust/soict/cysec/oop/model/MainModel.java
+++ oop/src/main/java/hust/soict/cysec/oop/model/MainModel.java
@@ -18,44 +18,44 @@ public class MainModel {
 	public MainModel() {
 		try {
 			Gson gson = new Gson();
-			
 			Dynasty dynastyTemp[] = null;
-			FileReader readerDynasty = new FileReader("oop/src/main/json/Dynasty.json");
+			FileReader readerDynasty = new FileReader("src/main/json/Dynasty.json");
 			dynastyTemp = gson.fromJson(readerDynasty, Dynasty[].class);
 			for (Dynasty dynasty : dynastyTemp) {
 				dynasties.add(dynasty);
+				System.out.println(dynasty);
 			}
 			
 			Festival festivalTemp[] = null;
-			FileReader readerFestival = new FileReader("oop/src/main/json/Festival.json");
+			FileReader readerFestival = new FileReader("src/main/json/Festival.json");
 			festivalTemp = gson.fromJson(readerFestival, Festival[].class);
 			for (Festival festival : festivalTemp) {
 				festivals.add(festival);
 			}
 			
 			HistoricalEvent eventTemp[] = null;
-			FileReader readerEvent = new FileReader("oop/src/main/json/Event.json");
+			FileReader readerEvent = new FileReader("src/main/json/Event.json");
 			eventTemp = gson.fromJson(readerEvent, HistoricalEvent[].class);
 			for (HistoricalEvent event : eventTemp) {
 				events.add(event);
 			}
 			
 			Figure figureTemp[] = null;
-			FileReader readerFigure = new FileReader("oop/src/main/json/Figures.json");
+			FileReader readerFigure = new FileReader("src/main/json/Figures.json");
 			figureTemp = gson.fromJson(readerFigure, Figure[].class);
 			for (Figure figure : figureTemp) {
 				figures.add(figure);
 			}
 			
 			King kingTemp[] = null;
-			FileReader readerKing = new FileReader("oop/src/main/json/Kings.json");
+			FileReader readerKing = new FileReader("src/main/json/Kings.json");
 			kingTemp = gson.fromJson(readerKing, King[].class);
 			for (King king : kingTemp) {
 				kings.add(king);
 			}
 			
 			Relic relicTemp[] = null;
-			FileReader readerRelic = new FileReader("oop/src/main/json/Relic.json");
+			FileReader readerRelic = new FileReader("src/main/json/Relic.json");
 			relicTemp = gson.fromJson(readerRelic, Relic[].class);
 			for (Relic relic : relicTemp) {
 				relics.add(relic);
